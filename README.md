81e178161072d88f1972ad94b2289a0d7d023aef
Loan Vaque <loanvaque@users.noreply.github.com>
2018-12-23 17:34:37 +0100

    Added traceroute output parsing

--- README.md
+++ README.md
@@ -1,29 +1,30 @@
 # Parse 2 Json
 
-Parsing of output files to json strings
+Parsing of shell commands output to json strings
 
 ## Process
 
 1. Scan directory recursively
 2. Find file names matching a pattern from a list of recognised patterns
-3. Parse the content of the file according to the matched pattern
+3. Parse the content of the file according to the matched pattern in the file name
 4. Convert the obtained elements to a json string
-5. Save the json string to a file with the extenion ".json"
+5. Write the json string to a file with the extenion ".json"
 
 ## File names
 
 The names of the files to be parsed must contain one of the expected patterns\
-Example: "github_ns_records**.dig.**txt"
+Example: "github_ns_records.dig.txt" (".dig")
 
 The names of the output files will be the same as the names of the original files, appended with the extension ".json"\
-Example: "github_ns_records.dig.txt**.json**"
+Example: "github_ns_records.dig.txt.json" (".json")
 
 ## Regex patterns
 
 File contents currently parseable:
 
-- **nmap** (https://en.wikipedia.org/wiki/Nmap)
-- **dig** (https://en.wikipedia.org/wiki/Dig_(command))
+- **nmap** (https://linux.die.net/man/1/nmap)
+- **dig** (https://linux.die.net/man/1/dig)
+- **traceroute** (https://linux.die.net/man/8/traceroute)
 
 More regex patterns will be added according to personal needs or requests
 
@@ -38,47 +39,49 @@ Output of a dig command:
 ; <<>> DiG 9.10.6 <<>> github.com +nostats
 ;; global options: +cmd
 ;; Got answer:
-;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 21216
-;; flags: qr rd ra; QUERY: 1, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 1
+;; ->>HEADER<<- opcode: QUERY, status: NOERROR, id: 60876
+;; flags: qr rd ra ad; QUERY: 1, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 0
 
-;; OPT PSEUDOSECTION:
-; EDNS: version: 0, flags:; udp: 512
 ;; QUESTION SECTION:
 ;github.com.            IN  A
 
 ;; ANSWER SECTION:
-github.com.     32  IN  A   140.82.118.4
-github.com.     32  IN  A   140.82.118.3
+github.com.     59  IN  A   140.82.118.4
+github.com.     59  IN  A   140.82.118.3
 ```
 
 Parsed into the following json string:
 
 ```
 {
-    "command": "dig github.com +nostats",
+    "command": {
+        "string": "dig github.com +nostats",
+        "version": "9.10.6"
+    },
     "recordList": [
         {
             "host": "github.com.",
-            "ttl": "32",
-            "recordType": "A",
+            "ttl": "59",
+            "type": "A",
             "record": "140.82.118.4"
         },
         {
             "host": "github.com.",
-            "ttl": "32",
-            "recordType": "A",
+            "ttl": "59",
+            "type": "A",
             "record": "140.82.118.3"
         }
-    ]
+    ],
+    "parser": {
+        "name": "parse2Json",
+        "version": "0.1.0",
+        "rawInput": "\n; \u003c\u003c\u003e\u003e DiG 9.10.6 \u003c\u003c\u003e\u003e github.com +nostats\n;; global options: +cmd\n;; Got answer:\n;; -\u003e\u003eHEADER\u003c\u003c- opcode: QUERY, status: NOERROR, id: 58248\n;; flags: qr rd ra; QUERY: 1, ANSWER: 2, AUTHORITY: 0, ADDITIONAL: 1\n\n;; OPT PSEUDOSECTION:\n; EDNS: version: 0, flags:; udp: 512\n;; QUESTION SECTION:\n;github.com.\t\t\tIN\tA\n\n;; ANSWER SECTION:\ngithub.com.\t\t59\tIN\tA\t140.82.118.4\ngithub.com.\t\t59\tIN\tA\t140.82.118.3\n\n"
 }
 ```
 
-Saved in a file with the extension ".json"
-
 ## Versioning
 
-Versioning is based on the RFC1035 recommendations for SOA RDATA format: YYYYMMDDnn\
-Example: 2018121301 (year 2018, month 12, day 13, serial 01)
+Versioning is based on [SemVer](http://semver.org/)
 
 ## License
 
--- parse2Json.go
+++ parse2Json.go
@@ -9,14 +9,17 @@ import (
 )
 
 const (
-	CODE_VERSION = "2018121301"
+	PARSER_NAME = "parse2Json"
+	PARSER_VERSION = "0.1.0"
 )
 
-// function to parse the output of an nmap scan to a json string
+// function to parse the output of an nmap command to a json string
 func parseNmap(fileContent []byte) []byte {
-	type NmapScan struct {
-		Command string `json:"command"`
-		Version string `json:"version"`
+	type NmapOutput struct {
+		Command struct {
+			String string `json:"string"`
+			Version string `json:"version"`
+		} `json:"command"`
 		Os string `json:"os"`
 		HostList []struct {
 			IpAddress string `json:"ipAddress"`
@@ -30,80 +33,185 @@ func parseNmap(fileContent []byte) []byte {
 			Service string `json:"service"`
 			Version string `json:"version"`
 		} `json:"portList"`
+		Parser struct {
+			Name string `json:"name"`
+			Version string `json:"version"`
+			RawInput string `json:"rawInput"`
+		} `json:"parser"`
 	}
 
-	nmapScan := NmapScan{}
+	nmapOutput := NmapOutput{}
 
-	regexCommand, err := regexp.Compile("# Nmap ([0-9.]+) scan initiated .+ as: ([^\n]+)")
+	regexHeader, err := regexp.Compile("# Nmap ([0-9.]+) scan initiated .+ as: ([^\n]+)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
-	command := regexCommand.FindStringSubmatch(string(fileContent))
-	if len(command) > 1 {
-		nmapScan.Version = command[1]
-		nmapScan.Command = command[2]
+	header := regexHeader.FindStringSubmatch(string(fileContent))
+	if len(header) > 1 {
+		nmapOutput.Command.Version = header[1]
+		nmapOutput.Command.String = header[2]
 	}
 
 	regexOs, err := regexp.Compile("Service Info: OS: ([^\n]+)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
 	os := regexOs.FindStringSubmatch(string(fileContent))
-	if len(os) > 0 { nmapScan.Os = os[1] }
+	if len(os) > 0 { nmapOutput.Os = os[1] }
 
-	regexHostList, err := regexp.Compile("Nmap scan report for ([0-9.]+)\nHost is up \\(([0-9\\.]+)s latency\\).\nMAC Address: ([0-9A-Fa-f:]{17}) \\(([^\\)]+)\\)")
+	regexHostList, err := regexp.Compile("Nmap scan report for ([0-9.]{7,15})\nHost is up \\(([0-9\\.]+)s latency\\).\nMAC Address: ([0-9A-Fa-f:]{17}) \\(([^\\)]+)\\)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
 	hostList := regexHostList.FindAllStringSubmatch(string(fileContent), -1)
 	if len(hostList) > 0 && len(hostList[0]) > 4 {
-		for _, host := range hostList { nmapScan.HostList = append(nmapScan.HostList, struct{IpAddress string `json:"ipAddress"`; Latency string `json:"latency"`; MacAddress string `json:"macAddress"`; Maker string `json:"maker"`}{host[1], host[2], host[3], host[4]}) }
+		for _, host := range hostList {
+			hostJson := struct{
+				IpAddress string `json:"ipAddress"`
+				Latency string `json:"latency"`
+				MacAddress string `json:"macAddress"`
+				Maker string `json:"maker"`
+			}{host[1], host[2], host[3], host[4]}
+			nmapOutput.HostList = append(nmapOutput.HostList, hostJson) }
 	}
 
 	regexPortList, err := regexp.Compile("([0-9]+/[a-z]+)[ \t]+([a-z]+)[ \t]+([^ \n]+)[ \t]*([^\n]*)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
 	portList := regexPortList.FindAllStringSubmatch(string(fileContent), -1)
 	if len(portList) > 0 && len(portList[0]) > 4 {
-		for _, port := range portList { nmapScan.PortList = append(nmapScan.PortList, struct{Port string `json:"port"`; State string `json:"state"`; Service string `json:"service"`; Version string `json:"version"`}{port[1], port[2], port[3], port[4]}) }
+		for _, port := range portList {
+			portJson := struct{
+				Port string `json:"port"`
+				State string `json:"state"`
+				Service string `json:"service"`
+				Version string `json:"version"`
+			}{port[1], port[2], port[3], port[4]}
+			nmapOutput.PortList = append(nmapOutput.PortList, portJson) }
 	}
 
-	jsonString, err := json.MarshalIndent(nmapScan, "", "    ")
+	nmapOutput.Parser.Name = PARSER_NAME
+	nmapOutput.Parser.Version = PARSER_VERSION
+	nmapOutput.Parser.RawInput = string(fileContent)
+
+	jsonString, err := json.MarshalIndent(nmapOutput, "", "    ")
 	if err != nil { fmt.Printf("Error: unable to marshal > %v\n", err); return nil }
 
 	return jsonString
 }
 
-// function to parse the output of a dig query to a json string
+// function to parse the output of a dig command to a json string
 func parseDig(fileContent []byte) []byte {
-	type DigQuery struct {
-		Command string `json:"command"`
-		Version string `json:"version"`
+	type DigOutput struct {
+		Command struct {
+			String string `json:"string"`
+			Version string `json:"version"`
+		} `json:"command"`
 		RecordList []struct {
 			Host string `json:"host"`
 			Ttl string `json:"ttl"`
 			Type string `json:"type"`
 			Record string `json:"record"`
 		} `json:"recordList"`
+		Parser struct {
+			Name string `json:"name"`
+			Version string `json:"version"`
+			RawInput string `json:"rawInput"`
+		} `json:"parser"`
 	}
 
-	digQuery := DigQuery{}
+	digOutput := DigOutput{}
 
 	regexHeader, err := regexp.Compile("; <<>> DiG ([0-9.]+) <<>> ([^\n]+)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
 	header := regexHeader.FindStringSubmatch(string(fileContent))
 	if len(header) > 1 {
-		digQuery.Version = header[1]
-		digQuery.Command = "dig " + header[2]
+		digOutput.Command.Version = header[1]
+		digOutput.Command.String = "dig " + header[2]
 	}
 
 	regexRecordList, err := regexp.Compile("\n([a-z.]+)[ \t]+([0-9]+)[ \t]+IN[ \t]+([A-Z]+)[ \t]+([^\n]+)")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
 	recordList := regexRecordList.FindAllStringSubmatch(string(fileContent), -1)
 	if len(recordList) > 0 && len(recordList[0]) > 4 {
-		for _, record := range recordList { digQuery.RecordList = append(digQuery.RecordList, struct{Host string `json:"host"`; Ttl string `json:"ttl"`; Type string `json:"type"`; Record string `json:"record"`}{record[1], record[2], record[3], record[4]}) }
+		for _, record := range recordList {
+			recordJson := struct{
+				Host string `json:"host"`
+				Ttl string `json:"ttl"`
+				Type string `json:"type"`
+				Record string `json:"record"`
+			}{record[1], record[2], record[3], record[4]}
+			digOutput.RecordList = append(digOutput.RecordList, recordJson) }
 	}
 
-	jsonString, err := json.MarshalIndent(digQuery, "", "    ")
+	digOutput.Parser.Name = PARSER_NAME
+	digOutput.Parser.Version = PARSER_VERSION
+	digOutput.Parser.RawInput = string(fileContent)
+
+	jsonString, err := json.MarshalIndent(digOutput, "", "    ")
 	if err != nil { fmt.Printf("Error: unable to marshal > %v\n", err); return nil }
 
 	return jsonString
 }
 
-// function to recursively scan a directory structure looking for files to parse (.json extensions and names starting with a dot "." are ignored)
+
+// function to parse the output of a traceroute command to a json string
+func parseTraceroute(fileContent []byte) []byte {
+	type TracerouteOutput struct {
+		Command struct {
+			String string `json:"string"`
+			Target struct {
+				HostName string `json:"hostName"`
+				IpAddress string `json:"ipAddress"`
+			} `json:"target"`
+			MaxHops string `json:"maxHops"`
+			PacketSize string `json:"packetSize"`
+		} `json:"command"`
+		HopList []struct {
+			HopNumber string `json:"hopNumber"`
+			HostName string `json:"hostName"`
+			IpAddress string `json:"ipAddress"`
+			Latency string `json:"latency"`
+		} `json:"hopList"`
+		Parser struct {
+			Name string `json:"name"`
+			Version string `json:"version"`
+			RawInput string `json:"rawInput"`
+		} `json:"parser"`
+	}
+
+	tracerouteOutput := TracerouteOutput{}
+
+	regexHeader, err := regexp.Compile("traceroute to ([a-z0-9.-]+) \\(([0-9.]+)\\), ([0-9]+) hops max, ([0-9]+) byte packets")
+	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
+	header := regexHeader.FindStringSubmatch(string(fileContent))
+	if len(header) > 5 {
+		tracerouteOutput.Command.String = header[1]
+		tracerouteOutput.Command.Target.HostName = header[2]
+		tracerouteOutput.Command.Target.IpAddress = header[3]
+		tracerouteOutput.Command.MaxHops = header[4]
+		tracerouteOutput.Command.PacketSize = header[5]
+	}
+
+	regexHopList, err := regexp.Compile("\n *([0-9]+) {2}([a-z0-9.-]+) \\(([0-9.]+)\\) {2}([0-9.]+) ms")
+	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return nil }
+	hopList := regexHopList.FindAllStringSubmatch(string(fileContent), -1)
+	if len(hopList) > 0 && len(hopList[0]) > 4 {
+		for _, hop := range hopList {
+			hopJson := struct{
+				HopNumber string `json:"hopNumber"`
+				HostName string `json:"hostName"`
+				IpAddress string `json:"ipAddress"`
+				Latency string `json:"latency"`
+			}{hop[1], hop[2], hop[3], hop[4]}
+			tracerouteOutput.HopList = append(tracerouteOutput.HopList, hopJson)
+		}
+	}
+
+	tracerouteOutput.Parser.Name = PARSER_NAME
+	tracerouteOutput.Parser.Version = PARSER_VERSION
+	tracerouteOutput.Parser.RawInput = string(fileContent)
+
+	jsonString, err := json.MarshalIndent(tracerouteOutput, "", "    ")
+	if err != nil { fmt.Printf("Error: unable to marshal > %v\n", err); return nil }
+
+	return jsonString
+}
+
+// function to recursively scan a directory structure looking for files (files with extension .json and files/dirs prefixed with a dot "." are ignored)
 func dirScan(dirPath string) []string {
 	var fileList []string
 	files, err := ioutil.ReadDir(dirPath)
@@ -123,9 +231,12 @@ func dirScan(dirPath string) []string {
 func main() {
 	dirPath := "."
 
-	regexNmap, err := regexp.Compile(".nmap.") // regex string to identify file names containing a nmap tag
+	// regexes to select each different command output file
+	regexNmap, err := regexp.Compile(".nmap")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return }
-	regexDig, err := regexp.Compile(".dig.") // regex string to identify file names containing a dig tag
+	regexDig, err := regexp.Compile(".dig")
+	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return }
+	regexTraceroute, err := regexp.Compile(".traceroute")
 	if err != nil { fmt.Printf("Error: unable to compile regex > %v\n", err); return }
 
 	fileList := dirScan(dirPath)
@@ -133,11 +244,14 @@ func main() {
 		fileContent, err := ioutil.ReadFile(filePath)
 		if err != nil { fmt.Printf("Error: unable to read file > %v\n", err); return }
 
+		// TODO: add check for already parsed .json file
 		var jsonString []byte = nil
 		if regexNmap.MatchString(filePath) == true {
 			jsonString = parseNmap(fileContent)
 		} else if regexDig.MatchString(filePath) == true {
 			jsonString = parseDig(fileContent)
+		} else if regexTraceroute.MatchString(filePath) == true {
+			jsonString = parseTraceroute(fileContent)
 		}
 
 		if jsonString != nil {
